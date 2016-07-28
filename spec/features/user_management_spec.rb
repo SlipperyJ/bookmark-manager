3f5f097e7cd1de2cf9372a577193db68ddfb8bca
Joe Sweeny <joesweeny84@hotmail.com>
2016-07-28 16:45:55 +0100

    Ch21 - user not created with invalid email

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -5,8 +5,12 @@ feature 'User sign up' do
     expect(current_path).to eq '/users'
     expect(page).to have_content('Password and confirmation password do not match')
   end
+
+  scenario 'user cannot sign up with blank email address' do
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+  end
+
+  scenario 'user cannot sign up with an invalid email address' do
+    expect { sign_up(email: 'invalid email') }.not_to change(User, :count)
+  end
 end
-
-
-
-# xpect(current_path).to eq '/links'
