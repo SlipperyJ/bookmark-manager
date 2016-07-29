14962365eac8afd95c9fa77acd7a655469398b49
Joe Sweeny <joesweeny84@hotmail.com>
2016-07-29 10:45:49 +0100

    Ch22 - user cannot sign up with existing email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User
   attr_accessor :password_confirmation
 
   property :id, Serial
-  property :email, String, format: :email_address, required: true
+  property :email, String, format: :email_address, required: true, unique: true
   property :password_digest, String, length: 60
 
   def password=(password)
@@ -17,5 +17,6 @@ class User
   end
 
   validates_confirmation_of :password
+  validates_uniqueness_of :email
 
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -3,14 +3,22 @@ feature 'User sign up' do
     sign_up
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq '/users'
-    expect(page).to have_content('Password and confirmation password do not match')
+    expect(page).to have_content('Password does not match the confirmation')
   end
 
   scenario 'user cannot sign up with blank email address' do
     expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(page).to have_content('Email must not be blank')
   end
 
   scenario 'user cannot sign up with an invalid email address' do
     expect { sign_up(email: 'invalid email') }.not_to change(User, :count)
+    expect(page).to have_content('Email has an invalid format')
+  end
+
+  scenario 'user cannot sign up with previously registered email' do
+    sign_up
+    expect { sign_up }.not_to change(User, :count)
+    expect(page).to have_content('Email is already taken')
   end
 end
