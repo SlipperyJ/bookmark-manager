3054ea4bb1c367195cd45b6ad9a32647654151c0
Joe Sweeny <joesweeny84@hotmail.com>
2016-07-29 12:22:49 +0100

    Ch23 - Sign In

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -1,15 +1,37 @@
 require 'spec_helper'
 
 feature 'User sign in' do
-  before(:each) do
-    sign_up
+
+  let!(:user) do
+    User.create(email: 'user@example.com',
+                password: 'secret1234',
+                password_confirmation: 'secret1234')
   end
 
-  scenario 'can sign in a user' do
-    visit '/users/sign_in'
-    fill_in 'email', with: 'albie@albie.com'
-    fill_in 'password', with: 'joe1'
-    click_button 'Sign In'
-    expect(page).to have_content('Welcome, albie@albie.com')
+  scenario 'with correct credentials' do
+    sign_in(email: user.email,   password: user.password)
+    expect(page).to have_content "Welcome, #{user.email}"
   end
+
+  def sign_in(email:, password:)
+    visit '/sessions/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    click_button 'Sign in'
+  end
+
+  let!(:user) do
+    User.create(email: 'test@test.com', password: 'secret1234',
+               password_confirmation: 'secret1234')
+  end
+
+  it 'authenticates when given a valid email address and password' do
+    authenticated_user = User.authenticate(user.email, user.password)
+    expect(authenticated_user).to eq user
+  end
+
+  it 'does not authenticate when given an incorrect password' do
+    expect(User.authenticate(user.email, 'wrong_stupid_password')).to be_nil
+  end
+
 end
